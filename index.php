fc908cee96caccd3247eebb60f86688e7453eeab
Unknown <ant.guillard@gmail.com>
2020-07-29 16:29:27 +0200

    minupdate

--- index.php
+++ index.php
@@ -4,26 +4,14 @@ use App\Router\Router;
 
 define('ROOT', dirname(__DIR__).'/shukidy');
 require ROOT . '/app/Manager.php';
-Manager::load();
 
-if (isset($_GET['url'])) {
 
-	$router = new Router($_GET['url']);
 
-	$router->get('/', function(){ echo "Accueil"; });
-
-	$router->get('/aventures', function(){ echo "Index de toutes les aventures"; });
-
-	$router->get('/aventures/:id', function($id){ echo "Ca c'est l'aventure ".$id; });
-
-	$router->post('/aventure/:id', function($id){ echo "Ca je sais pas encore trop à quoi ça sert mais l'id c'est ".$id; });
-
-	$router->run();
-
-}
 
 
 
 
 
 ?>
+
+coucouuuu
